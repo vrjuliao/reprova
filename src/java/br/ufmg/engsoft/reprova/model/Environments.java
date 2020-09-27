0ee69e618e44d6ba182d9beba1269f2dd6fa0e6a
Gustavo Pereira <ghapereira>
2020-09-26 21:46:42 -0300

    Add multiple choice to question

--- src/java/br/ufmg/engsoft/reprova/model/Environments.java
+++ src/java/br/ufmg/engsoft/reprova/model/Environments.java
@@ -3,63 +3,73 @@ package br.ufmg.engsoft.reprova.model;
 import java.util.Optional;
 
 public class Environments {
-	private static Environments environments;
-	
-	private String token;
-	private int port;
-	
-	private int difficultyGroup;
-	
-	private boolean enableAnswers;
+    private static Environments environments;
+
+    private String token;
+    private int port;
+
+    private int difficultyGroup;
+
+    private boolean enableAnswers;
     private boolean enableQuestionnaires;
-	
-	private Environments() {		
-		Optional<String> enableAnswersEnv = Optional.ofNullable(System.getenv("ENABLE_ANSWERS"));
-		enableAnswersEnv.ifPresentOrElse(
-				enableAnswers -> this.enableAnswers = enableAnswers.toLowerCase().equals("true"),
-				() -> this.enableAnswers = false);
-		
-		Optional<String> enableQuestionnairesEnv = Optional.ofNullable(System.getenv("ENABLE_QUESTIONNAIRES"));
+    private boolean enableMultipleChoice;
+
+    private Environments() {
+        Optional<String> enableAnswersEnv = Optional.ofNullable(System.getenv("ENABLE_ANSWERS"));
+        enableAnswersEnv.ifPresentOrElse(
+                enableAnswers -> this.enableAnswers = enableAnswers.toLowerCase().equals("true"),
+                () -> this.enableAnswers = false);
+
+        Optional<String> enableQuestionnairesEnv = Optional.ofNullable(System.getenv("ENABLE_QUESTIONNAIRES"));
         enableQuestionnairesEnv.ifPresentOrElse(
                 enableQuestionnaires -> this.enableQuestionnaires = enableQuestionnaires.toLowerCase().equals("true"),
                 () -> this.enableQuestionnaires = false);
-		
-		Optional<String> envDifficultyGroup = Optional.ofNullable(System.getenv("DIFFICULTY_GROUP"));
-		envDifficultyGroup.ifPresentOrElse(
-		        difficultyGroup -> this.difficultyGroup = Integer.parseInt(envDifficultyGroup.get()),
-		        () -> this.difficultyGroup = 0);
-		
-		this.port = Integer.parseInt(System.getenv("PORT"));
-		
-		this.token = System.getenv("REPROVA_TOKEN");
-	}
-	
-	public static Environments getInstance() {
-		if (environments == null) {
-			environments = new Environments();
-		}
-		
-		return environments;
-	}
-	
-	public boolean getEnableAnswers() {
-		return this.enableAnswers;
-	}
-	
-	public boolean getEnableQuestionnaires() {
-	    return this.enableQuestionnaires;
-	}
-	
-	public int getDifficultyGroup() {
-	    return this.difficultyGroup;
-	}
-	
-	public String getToken() {
-	    return this.token;
-	}
-	
-	public int getPort() {
-	    return this.port;
-	}
+
+        Optional<String> envDifficultyGroup = Optional.ofNullable(System.getenv("DIFFICULTY_GROUP"));
+        envDifficultyGroup.ifPresentOrElse(
+                difficultyGroup -> this.difficultyGroup = Integer.parseInt(envDifficultyGroup.get()),
+                () -> this.difficultyGroup = 0);
+
+        Optional<String> enableMultipleChoiceEnv = Optional.ofNullable(System.getenv("ENABLE_MULTIPLE_CHOICE"));
+        enableMultipleChoiceEnv.ifPresentOrElse(
+                enableMultipleChoice -> this.enableMultipleChoice = enableMultipleChoice.toLowerCase().equals("true"),
+                () -> this.enableMultipleChoice = false);
+
+        this.port = Integer.parseInt(System.getenv("PORT"));
+
+        this.token = System.getenv("REPROVA_TOKEN");
+    }
+
+    public static Environments getInstance() {
+        if (environments == null) {
+            environments = new Environments();
+        }
+
+        return environments;
+    }
+
+    public boolean getEnableAnswers() {
+        return this.enableAnswers;
+    }
+
+    public boolean getEnableQuestionnaires() {
+        return this.enableQuestionnaires;
+    }
+
+    public int getDifficultyGroup() {
+        return this.difficultyGroup;
+    }
+
+    public boolean getEnableMultipleChoice() {
+        return this.enableMultipleChoice;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
 
 }
