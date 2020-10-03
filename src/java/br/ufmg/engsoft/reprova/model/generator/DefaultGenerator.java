91baa694800d5a7699a4dec9a12515af83b0cfef
henriqueup <titepessoa@gmail.com>
2020-10-03 13:09:50 -0300

    Default Generator

--- src/java/br/ufmg/engsoft/reprova/model/generator/DefaultGenerator.java
+++ src/java/br/ufmg/engsoft/reprova/model/generator/DefaultGenerator.java
@@ -14,8 +14,24 @@ import br.ufmg.engsoft.reprova.model.difficulty.DifficultyFactory;
 public class DefaultGenerator implements IQuestionnaireGenerator{
 
   public Questionnaire generate(QuestionsDAO questionsDAO, String averageDifficulty, int questionsCount, int totalEstimatedTime){
-    //implement default questionnaire generation
+    if (totalEstimatedTime == 0){
+      totalEstimatedTime = Questionnaire.DEFAULT_ESTIMATED_TIME_MINUTES;
+    }
+    if (questionsCount == 0){
+      questionsCount = Questionnaire.DEFAULT_QUESTIONS_COUNT;
+    }
+
     ArrayList<Question> questions = new ArrayList<Question>();
+    ArrayList<Question> allQuestions = new ArrayList<Question>(questionsDAO.list(null, null));
+
+    Collections.shuffle(allQuestions);
+    for (int i = 0; i < questionsCount; i++){
+      if (i >=  allQuestions.size()){
+        break;
+      }
+
+      questions.add(allQuestions.get(i));
+    }
 
     return new Questionnaire.Builder()
                 .averageDifficulty(averageDifficulty)
