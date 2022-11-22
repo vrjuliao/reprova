9c0f12252db89373597fbd47d4595614c3681ce6
guilhermeleroy <leroy.luiz@gmail.com>
2022-11-22 20:15:22 -0300

    Replacing system.out with logger

--- src/java/br/ufmg/engsoft/reprova/Reprova.java
+++ src/java/br/ufmg/engsoft/reprova/Reprova.java
@@ -8,14 +8,19 @@ import br.ufmg.engsoft.reprova.routes.Setup;
 import br.ufmg.engsoft.reprova.mime.json.Json;
 import br.ufmg.engsoft.reprova.model.Environments;
 
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 public class Reprova {
+
+    private static Logger logger = Logger.getLogger(Reprova.class.getName());
     public static void main(String[] args) {
         Mongo mongoDB;
 
         try {
             mongoDB = new Mongo("reprova");
         } catch (Exception e) {
-            System.out.println(e);
+            logger.log(Level.INFO, "Caught exception", e);
             return;
         }
 
